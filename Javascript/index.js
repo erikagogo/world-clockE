01923890ea58837b050d01abc49173469ad80a94
erikagogo <erika.gogo@gmail.com>
2025-08-19 14:14:02 -0400

    Added Javascript function to the project

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,29 @@
+function updateLosAngelesTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss:SS [<small>]A[</small>]"
+  );
+}
+
+updateLosAngelesTime();
+setInterval(updateLosAngelesTime, 1);
+
+function updateSydneyTime() {
+  let SydneyElement = document.querySelector("#sydney");
+  let SydneyDateElement = SydneyElement.querySelector(".date");
+  let SydneyTimeElement = SydneyElement.querySelector(".time");
+  let SydneyTime = moment().tz("Australia/Sydney");
+
+  SydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  SydneyTimeElement.innerHTML = SydneyTime.format(
+    "h:mm:ss:SS [<small>]A[</small>]"
+  );
+}
+
+updateSydneyTime();
+setInterval(updateSydneyTime, 1);
