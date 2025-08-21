8a7faa4039f22580edc0f4bf5b6b1ee15505ae8f
erikagogo <erika.gogo@gmail.com>
2025-08-21 11:00:07 -0400

    Added link to all cities

--- Javascript/index.js
+++ Javascript/index.js
@@ -42,7 +42,7 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "hh:mm:ss"
           )} <small>${cityTime.format("A")} </small></div>
-        </div> `;
+        </div> <a href="/">All cities</a> `;
 }
 
 updateTime();
